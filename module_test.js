55709ff9adb3cbc6f7bb17fcc85239fcfb9c27c1
adisonkh <noreply>
2015-10-13 02:15:15 +0000

    Add module module_test

---
+++ module_test.js
@@ -0,0 +1,15 @@
+/**
+ * @module //module_test
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Module_test
+ * @extends Montage
+ */
+exports.Module_test = Montage.specialize(/** @lends Module_test# */ {
+    constructor: {
+        value: function Module_test() {
+            this.super();
+        }
+    }
+});
